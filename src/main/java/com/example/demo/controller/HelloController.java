27fb77cf299dccc3bf02a12b472259f2cdd9ff41
TangBo <1263656881@qq.com>
2019-03-26 15:44:02 +0800

    RBAC角色权限系统

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
